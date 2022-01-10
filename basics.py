11ce3ba57fec2241ee8a41e8de6c120b8dd2ba01
Rose-Petals <91221979+Rose-Petals@users.noreply.github.com>
2022-01-10 17:05:07 -0600

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,4 +1,16 @@
-day_hours = 24
+day_hours = 24 
 week_days = 7
+#variable is a presentation of an object
 
-print(day_hours*week_days)
+week_hours = day_hours*week_days
+print(week_hours) 
+
+#skipped lines are just for readibilty and the program will ignore them
+#variables make code readable and expand the program
+#variables are used as transporters, they transport objects 
+
+
+#if you don't supply a file to the python command, python will open in an on demand mode
+#python shells are for testing things out and .py files are for writing code
+
+#use cmd instead of opening Video Studio code to use as a calculator
