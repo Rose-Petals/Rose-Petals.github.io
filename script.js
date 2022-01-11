021d0f042126ba84b4eabd565f7815335ef5e88f
Rose-Petals <91221979+Rose-Petals@users.noreply.github.com>
2022-01-11 17:06:37 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let myButton = document.getElementById("my-button");
-let myText= document.getElementById("some-text");
-
-myButton.onclick = function() {
-    myText.innerText += "\n You clicked the button!";
-    myText.style.backgroundColor = "lavender";
-    myText.style.color = "grey";
-};
