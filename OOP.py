7ef66aa7b2f5795f2682dcd36fb03a0356217bf0
Rose-Petals <91221979+Rose-Petals@users.noreply.github.com>
2022-05-31 04:40:17 -0500

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,3 @@
+#Object Oriented Programming is a way to organize your code
+#can organize functions used by a certain object into class
+
---
+++ UsingPythonWithCSV,JSON,andExcel.py
@@ -0,0 +1,6 @@
+#downloaded jupyter 
+#made a Jupyter Testing folder in my Python folder
+#pressed ALT + D 
+#wrote    jupyter notebook     which opens up the jupyter notebook window
+
+#syntax for minimal object -    def __init__():
